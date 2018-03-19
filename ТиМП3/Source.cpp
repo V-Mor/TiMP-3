747c4723b722acf8a3d4fee55b90ed07719cbdfd
V-Mor <vvovv266@gmail.com>
2018-03-19 15:26:57 +0700

    first version of Merge sort

--- ТиМП3/Source.cpp
+++ ТиМП3/Source.cpp
@@ -1,16 +1,11 @@
 #include<iostream>
 using namespace std;
 void Qsort(int*, int*);
+int* MergeSort(int*, int*);
 
 int main()
 {
-	int mass[10];
-	for (int i = 0; i <= 9; i++)
-		mass[i] = 9 - i;
-	Qsort(mass, &(mass[9]));
-	for (int i = 0; i <= 9; i++)
-		cout << mass[i] << " ";
-	cout << endl;
+	cout << sizeof(int) << endl;
 	return 0;
 
 }
@@ -38,4 +33,51 @@ void Qsort(int *nach, int *kon)
 		Qsort(l, kon);
 	if (r > nach)
 		Qsort(nach, r);
+}
+
+int* MergeSort(int* nach, int* kon)
+{
+	int length = (kon - nach) + 1;
+	int *opor = nach + (abs(length / 2));
+	if (length == 1)
+	{
+		return nach;
+	}
+	if (length == 2)
+	{
+		if (*nach < *kon)
+			swap(*nach, *kon);
+		return nach;
+	}
+	int* tempMass = new int[length];
+	int count = 0;
+	for (int *i = MergeSort(nach, opor - 1); i <= opor; i++)
+	{
+		for (int *j = MergeSort((opor), kon); j <= kon; j++)
+		{
+			if (*i < *j)
+			{
+				tempMass[count] = *i;
+				count++;
+				if (i == opor)
+					for (int *k = j; k <= kon; k++)
+					{
+						tempMass[count] = *k;
+						count++;
+					}
+				break;
+			}
+			if (*j < *i)
+			{
+				tempMass[count] = *j;
+				count++;
+			}
+			
+		}
+		if (count == length)
+			break;
+	}
+	return tempMass;
+
+
 }
